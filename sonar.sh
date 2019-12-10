031aae79649558025502a95ebe5103de12e7c959
BenHesketh21 <heskethben0@gmail.com>
2019-12-10 11:42:48 +0000

    Added branch

--- sonar.sh
+++ sonar.sh
@@ -8,6 +8,7 @@ done
 for name in "${november[@]}"
 do
 	git clone https://www.github.com/BenHesketh21/${name}.git
+	git checkout feature
 	cd ${name}
 	touch sonar-project.properties
 	echo "sonar.projectKey=${name}" > sonar-project.properties
