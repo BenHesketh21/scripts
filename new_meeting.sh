77c8ce2c2f245525f02b2d6345a89907dd9b4a7a
BenHesketh21 <heskethben0@gmail.com>
2019-12-11 15:53:18 +0000

    Added to meeting script

--- new_meeting.sh
+++ new_meeting.sh
@@ -5,6 +5,12 @@ read name
 echo "Title?"
 read title
 cd /home/ubuntu/trainer_meetings
+if [ -d "$(date "+%B-%y")" ]; then
+	cd $(date "+%B-%Y")
+else
+	mkdir $(date "+%B-%Y")
+	cd $(date "+%B-%Y")
+fi
 touch ${Name}.md
-echo "## ${title}-$(date "+%d-%m-%y")" >> ${name}.md
-
+echo "## ${title} Date:$(date "+%D")" >> ${name}.md
+vim ${name}.md
