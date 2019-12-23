fc04e7eb88cc9c75eef122db6928b51a8ba4d03b
BenHesketh21 <heskethben0@gmail.com>
2019-12-23 17:17:33 +0000

    Updated to do list

--- to_do.sh
+++ to_do.sh
@@ -7,8 +7,18 @@ else
 	mkdir $(date "+%B-%Y")
 	cd $(date "+%B-%Y")
 fi
-touch $(date "+%a-%d").md
-echo "## Date: $(date "+%D")" >> $(date "+%a-%d").md
-echo " " >> $(date "+%a-%d").md
-echo "- [ ] Task 1" >> $(date "+%a-%d").md
+if ! [ -f $(date "+%a-%-d").md ]
+then
+	echo "Hello"
+	touch $(date "+%a-%d").md
+	echo "## Date: $(date "+%D")" >> $(date "+%a-%d").md
+	echo " " >> $(date "+%a-%d").md
+	echo "- [ ] Task 1" >> $(date "+%a-%d").md
+elif ! [ -s $(date "+%a-%d").md ]
+then
+	echo "Hello 2"
+	echo "## Date: $(date "+%D")" >> $(date "+%a-%d").md
+        echo " " >> $(date "+%a-%d").md
+        echo "- [ ] Task 1" >> $(date "+%a-%d").md
+fi
 code $(date "+%a-%d").md
